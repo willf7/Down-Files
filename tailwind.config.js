1c0633ed163351dbfbe86f6feea58e24337930ff
William Felipe <williamfelipe081@gmail.com>
2024-01-20 14:48:12 -0300

    Tentativa de correcao do tailwind na vercel!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: [],
+  content: ["/src/*.{js,ts,jsx,tsx,html,css}"],
   theme: {
     colors: {
       transparent: "transparent",
